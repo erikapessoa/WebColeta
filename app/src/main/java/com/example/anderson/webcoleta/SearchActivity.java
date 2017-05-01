09105ebf113339b407df50e50f97de792ec7c655
danielle.nathalia@gmail.com <danielle.nathalia@gmail.com>
2017-04-30 21:22:39 -0300

    refazendo
    ;(

--- app/src/main/java/com/example/anderson/webcoleta/SearchActivity.java
+++ app/src/main/java/com/example/anderson/webcoleta/SearchActivity.java
@@ -2,8 +2,11 @@ package com.example.anderson.webcoleta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -22,18 +25,36 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        spinnerS = (Spinner)findViewById(R.id.spinner1);
+
+        spinnerT = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapterT = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, itensTurno);
+        adapterT.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerT.setAdapter(adapterT);
+
+        spinnerS = (Spinner)findViewById(R.id.spinner2);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, itensSetor);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerS.setAdapter(adapter);
 
+        spinnerS.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        spinnerT = (Spinner)findViewById(R.id.spinner2);
-        ArrayAdapter<String> adapterT = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, itensTurno);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerT.setAdapter(adapterT);
+                Toast.makeText(SearchActivity.this, "O item escolhido foi: ", Toast.LENGTH_SHORT).show();
+
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
 
 
 
-    }
-}
+
+            }
+        }
+
+
+
+

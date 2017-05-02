468c26f7e4de367fe9a5cf7fff628660bd547079
dinhoaps <dinhoaps@gmail.com>
2017-05-01 22:03:10 -0300

    Adição de busca por setor e turno
    Segue a busca

--- app/src/main/java/com/example/anderson/webcoleta/util/WebService.java
+++ app/src/main/java/com/example/anderson/webcoleta/util/WebService.java
@@ -22,6 +22,16 @@ import java.util.ArrayList;
 public class WebService  {
 
     private ArrayList<GarbagePlace> mArrGPI = new ArrayList<GarbagePlace>();
+    private ArrayList<GarbagePlace> mArrGPI2 = new ArrayList<GarbagePlace>();
+
+    private static int INTERVALO = 0;
+    private static int SETOR = 1;
+    private static int ENDERECO = 2;
+    private static int TURNO = 3;
+    private static int ROTASETOR = 4;
+    private static int FREQUENCIA = 5;
+
+
     private GarbagePlace[] mGarbagePlacles;
 
     public GarbagePlace[] readGarbagePlaces () {
@@ -53,12 +63,12 @@ public class WebService  {
                     if (count > 1) {
                         GarbagePlace mGarbagePlace = new GarbagePlace();
                         mGarbagePlace.setId(String.valueOf(count - 1));
-                        mGarbagePlace.setIntervalo(row[0]);
-                        mGarbagePlace.setSetor(row[1]);
-                        mGarbagePlace.setEndereco(row[2]);
-                        mGarbagePlace.setTurno(row[3]);
-                        mGarbagePlace.setRotaSetor(row[4]);
-                        mGarbagePlace.setFrequencia(row[5]);
+                        mGarbagePlace.setIntervalo(row[INTERVALO]);
+                        mGarbagePlace.setSetor(row[SETOR]);
+                        mGarbagePlace.setEndereco(row[ENDERECO]);
+                        mGarbagePlace.setTurno(row[TURNO]);
+                        mGarbagePlace.setRotaSetor(row[ROTASETOR]);
+                        mGarbagePlace.setFrequencia(row[FREQUENCIA]);
                         mArrGPI.add(mGarbagePlace);
                         //  Log.i("Valor = ", String.valueOf(count));
                     }
@@ -88,4 +98,81 @@ public class WebService  {
         //retorna o array contendo todos os pontos de coleta
         return mGarbagePlacles;
     }
+
+
+    public GarbagePlace[] readSetorGarbagePlaces (String letra) {
+
+        int opcao;
+
+        if(letra.length() > 1)
+            opcao = TURNO;
+        else
+            opcao = SETOR;
+
+        try {
+
+            URL url = new URL(Utils.sURL);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10 * 1000);
+            connection.setConnectTimeout(15 * 1000);
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.setDoOutput(false);
+            connection.connect();
+
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) { //conseguiu conectar
+
+
+                final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+
+                int count = 0;
+                while ((line = reader.readLine()) != null) {
+                    // "," ou ";" de acordo com o arquivo
+                    String[] row = line.split(";");
+                    count++;
+                    // Log.i("Linha: ", row[0]);
+                    if (count > 1) {
+
+                        if(row[opcao].toLowerCase().toString().contains(letra)) {
+                                 GarbagePlace mGarbagePlace = new GarbagePlace();
+                                 mGarbagePlace.setId(String.valueOf(count - 1));
+                                 mGarbagePlace.setIntervalo(row[INTERVALO]);
+                                 mGarbagePlace.setSetor(row[SETOR]);
+                                 mGarbagePlace.setEndereco(row[ENDERECO]);
+                                 mGarbagePlace.setTurno(row[TURNO]);
+                                 mGarbagePlace.setRotaSetor(row[ROTASETOR]);
+                                 mGarbagePlace.setFrequencia(row[FREQUENCIA]);
+                                mArrGPI2.add(mGarbagePlace);
+                        }
+                        //  Log.i("Valor = ", String.valueOf(count));
+                    }
+
+                }
+                //  mGGL = new GarbagePlaceGeosonList();
+                mGarbagePlacles = new GarbagePlace[mArrGPI2.size()];
+
+                for (int i = 0; i < mGarbagePlacles.length; i++) {
+                    mGarbagePlacles[i] = new GarbagePlace();
+                    mGarbagePlacles[i] = mArrGPI2.get(i);
+                    mGarbagePlacles[i].setId(Integer.toString(i));
+                }
+
+            } else {
+                mGarbagePlacles = null;
+                LogWrapper.log("Erro HTTP " + connection.getResponseCode());
+                //  Snackbar.make(mListPlaces, getString(R.string.connection_exception), Snackbar.LENGTH_LONG).show();
+            }
+        } catch (IOException e2) {
+            // Toast.makeText(ListPlacesActivity.this, R.string.connection_exception, Toast.LENGTH_LONG).show();
+            //Snackbar.make(mListPlaces, getString(R.string.connection_exception), Snackbar.LENGTH_LONG).show();
+            mGarbagePlacles = null;
+            Log.i("Exceção: ", e2.toString());
+        }
+
+        //retorna o array contendo todos os pontos de coleta
+        return mGarbagePlacles;
+    }
+
 }
